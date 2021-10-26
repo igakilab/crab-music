ee5af807552f8100e48ef748ac9437759a86b4e5
e1b19044 <72372407+e1b19044@users.noreply.github.com>
2021-10-26 22:42:07 +0900

    カニ画像差し替え他

---
+++ Crab music/Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,75 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CrabController::Start()
+extern void CrabController_Start_m1C22A35EC1363FA149C2C88BAD8485F98DCEA78D (void);
+// 0x00000002 System.Void CrabController::Update()
+extern void CrabController_Update_mC30C6C5477B2AA2C983CF98540859C432D2273E6 (void);
+// 0x00000003 System.Void CrabController::.ctor()
+extern void CrabController__ctor_mA7A54DACA8BAAC2DD0951EB9FA10D2090AF13B54 (void);
+// 0x00000004 System.Void TitleDirector::Update()
+extern void TitleDirector_Update_mD23AEF3E231E3DEE4B5102B42C2E04C3186D3DA1 (void);
+// 0x00000005 System.Void TitleDirector::.ctor()
+extern void TitleDirector__ctor_mCB89CF3051F858C7C140C1D78A7B2CB883F0817B (void);
+// 0x00000006 System.Void notesController::Start()
+extern void notesController_Start_m4D4B91F0CA512B1245047B595335648275657087 (void);
+// 0x00000007 System.Void notesController::Update()
+extern void notesController_Update_mD32A6B052059E5029AA367F0CE5CF054E4635E70 (void);
+// 0x00000008 System.Void notesController::.ctor()
+extern void notesController__ctor_m6076C7709BF961C9372DCFF05271677019591FE2 (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	CrabController_Start_m1C22A35EC1363FA149C2C88BAD8485F98DCEA78D,
+	CrabController_Update_mC30C6C5477B2AA2C983CF98540859C432D2273E6,
+	CrabController__ctor_mA7A54DACA8BAAC2DD0951EB9FA10D2090AF13B54,
+	TitleDirector_Update_mD23AEF3E231E3DEE4B5102B42C2E04C3186D3DA1,
+	TitleDirector__ctor_mCB89CF3051F858C7C140C1D78A7B2CB883F0817B,
+	notesController_Start_m4D4B91F0CA512B1245047B595335648275657087,
+	notesController_Update_mD32A6B052059E5029AA367F0CE5CF054E4635E70,
+	notesController__ctor_m6076C7709BF961C9372DCFF05271677019591FE2,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	851,
+	851,
+	851,
+	851,
+	851,
+	851,
+	851,
+	851,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
